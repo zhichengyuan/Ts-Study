25073fcb9cdcf89d64f066780e115b88cbaf4e3a
zhichengyuan <1798788388@qq.com>
2021-07-09 18:37:22 +0800

    yzc

--- TS-fuxi/src/index.ts
+++ TS-fuxi/src/index.ts
@@ -1,3 +1,10 @@
-let str:string = '12111'
-console.log(str);
+// let name:string;
+// name = '21';
 
+function add(a:number,b:number):number {
+    return a + b;
+}
+
+let sum:number = add(12,12);
+
+let name = 'kevin';
