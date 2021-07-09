7cff03668983f1d86735b5a9803f2e4eb73f66db
zhichengyuan <1798788388@qq.com>
2021-07-09 18:59:33 +0800

    yzc

--- TS-fuxi/src/index.ts
+++ TS-fuxi/src/index.ts
@@ -8,3 +8,20 @@ function add(a:number,b:number):number {
 let sum:number = add(12,12);
 
 let name = 'kevin';
+
+
+let nums:number[] = [3,4,2];
+let arr:Array<number>=[3,32,32]
+
+
+function printValues(obj:object) {
+    const vals = Object.values(obj);
+    vals.forEach( v => console.log(v))
+}
+
+printValues({
+    name:'dss',
+    age:12
+})
+
+// let n:string = undefined;
