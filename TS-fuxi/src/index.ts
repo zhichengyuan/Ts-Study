770525979479ab84c02d9ca8c3dc48b670b49333
zhichengyuan <1798788388@qq.com>
2021-07-12 22:18:10 +0800

    yzc

--- TS-fuxi/src/index.ts
+++ TS-fuxi/src/index.ts
@@ -5,7 +5,7 @@ function add(a:number,b:number):number {
     return a + b;
 }
 
-let sum:number = add(12,12);
+// let sum:number = add(12,12);
 
 let name = 'kevin';
 
@@ -45,3 +45,38 @@ function getUsers(g:Gender):User[] {
     return []
 }
 // let n:string = undefined;
+
+
+function combine(a:number,b:number):number;
+/**
+ * 得到a*b的结果
+ * @param a 
+ * @param b 
+ */
+function combine(a:string,b:string):string;
+/**
+ * 得到a和b的拼接的结果
+ * @param a 
+ * @param b 
+ */
+function combine(a:number | string,b:number|string):number | string {
+    if(typeof a === 'number' && typeof b === 'number') {
+        return a * b;
+    }
+    else if (typeof a === 'string' && typeof b === 'string') {
+        return a + b;
+    }
+    throw new Error('a和b必须是相同的类型');
+}
+
+const result = combine(2,2)
+
+function sum(a:number,b:number,c?:number) {
+    if(c) {
+        return a + b + c
+    }else {
+        return a + b;
+    }
+}
+sum(3,4)
+
