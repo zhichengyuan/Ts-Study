5ae5076a5cc6058bb97e9b57ae7c3fc007f19aea
zhichengyuan <1798788388@qq.com>
2021-07-13 20:31:10 +0800

    yzc

--- TS-fuxi/src/index.ts
+++ TS-fuxi/src/index.ts
@@ -1,4 +1,71 @@
-import { createDeck, printDeck } from "./funcs";
-const deck = createDeck();
-printDeck(deck);
+// import { createDeck, printDeck } from "./funcs";
+// const deck = createDeck();
+// printDeck(deck);
 
+// interface User {
+//     name:string,
+//     age:number,
+//     sayHello():void
+// }
+
+// let u:User ={
+//     name:'tom',
+//     age:33,
+//     sayHello(){
+//         console.log(this.name);
+//     }
+// }
+// u.sayHello();
+
+// type Condition = (n:number) => boolean
+
+// interface Condition { //定界符
+//     (n:number):boolean
+// }
+
+// function sum(numbers:number[],callBack:Condition) {
+//     let s = 0;
+//     numbers.forEach(n => {
+//         if(callBack(n)) {
+//             s +=n;
+//         }
+//     });
+//     return s
+// }
+
+// let res = sum([1,12],n=>n%2 !==0);
+// console.log(res)
+
+// interface A {
+//     T1:string
+// }
+
+// interface B extends A {
+//     T2:number
+// }
+
+// interface C extends A,B {
+//     T3:boolean
+// }
+
+// let u:B = {
+//     T2:12,
+//     T1:'wqw'
+// }
+
+interface A {
+    T1:string,
+}
+type B ={
+    T2:number
+}
+
+type C = {
+    T3:boolean
+} & A & B
+
+let u1:C = {
+    T2:21,
+    T1:'sdsd',
+    T3:true
+}
