cb640eefa32b5b4758ec988999b7b01b690fe390
zhichengyuan <1798788388@qq.com>
2021-07-13 11:09:27 +0800

    yzc

--- TS-fuxi/src/index.ts
+++ TS-fuxi/src/index.ts
@@ -1,57 +1,27 @@
-type Deck = NormalCard[];
-
-type Color = '♥'|'♠'|'♦'|'♣';
-
-type NormalCard = {
-    color:Color
-    mark:number
+enum Gender {
+    male = '男',
+    female = '女1'
 }
 
-function creatDeck():Deck {
-    let deck:Deck = [];
-    for(let i = 1;i <= 13;i++) {
-        deck.push({
-            mark:i,
-            color:"♠"
-        })
-        deck.push({
-            mark:i,
-            color:"♣"
-        })
-        deck.push({
-            mark:i,
-            color:"♥"
-        })
-        deck.push({
-            mark:i,
-            color:"♦"
-        })
-    }
-    return deck;
+
+
+let gender:Gender
+
+gender = Gender.male;
+gender = Gender.female
+
+
+enum Level {
+    level1,
+    level2,
+    level3
 }
 
-function printDeck(deck:Deck) {
-    let result = '\n'
-    deck.forEach((card,i) => {
-        let str = card.color;
-        if(card.mark<=10) {
-            str += card.mark;
-        }else if(card.mark === 11) {
-            str += 'J'
-        }else if(card.mark === 12) {
-            str += 'Q'
-        }else {
-            str += 'K'
-        }
-        result += str + '\t';
-        if((i + 1) % 6 ===0) {
-            result += '\n'
-        }
-        
+// console.log(gender);
 
-    })
-    console.log(result)
+function printGenders() {
+    const vals = Object.values(Gender);
+    vals.forEach(v => console.log(v))
 }
 
-const deck = creatDeck();
-printDeck(deck)
+printGenders();
