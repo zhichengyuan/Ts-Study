be85d2c39982a5a7cdd8a34c011b76fa46970f27
zhichengyuan <1798788388@qq.com>
2021-07-14 11:20:48 +0800

    yzc

--- TS-fuxi/src/index.ts
+++ TS-fuxi/src/index.ts
@@ -77,12 +77,12 @@
 //     age:number
 // }
 
-type User = {
-    readonly id:string
-    name:string
-    age:number
-    readonly arr:readonly string[]  //只读的数组
-}
+// type User = {
+//     readonly id:string
+//     name:string
+//     age:number
+//     readonly arr:readonly string[]  //只读的数组
+// }
 
 // let u:User ={
 //     id:'12',
@@ -93,3 +93,38 @@ type User = {
 // const arr:readonly number[] = [2,3,4];
 
 // const arr1:ReadonlyArray<number> = [4,3,4];
+
+// interface Duck {
+//     sound:'嘎嘎嘎'
+//     swin():void
+// }
+
+// let person = {
+//     name:'伪装成鸭子的人',
+//     age:11,
+//     sound:"嘎嘎嘎" as "嘎嘎嘎",
+//     swin() {
+//         console.log(this.name + '正在游泳，并发出了' + this.sound + '的声音')
+//     }
+// }
+
+// let duck:Duck = person
+
+
+
+interface Condition { //定界符
+    (n:number,i:number):boolean
+}
+
+function sum(numbers:number[],callBack:Condition) {
+    let s = 0;
+    numbers.forEach((n,i) => {
+        if(callBack(n,i)) {
+            s +=n;
+        }
+    });
+    return s
+}
+
+let res = sum([1,12,1,2,3,12],n=>n%2 !==0);
+console.log(res)
