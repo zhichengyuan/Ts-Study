83b2b0023bae285b2f3cb038cecb7139214ce7fb
zhichengyuan <1798788388@qq.com>
2021-07-14 21:28:15 +0800

    yzc

--- TS-fuxi/src/index.ts
+++ TS-fuxi/src/index.ts
@@ -8,16 +8,31 @@ class User {
     readonly id:number //不能改变
     gender:'男' | '女' = '男'
     pid?:string
-    private publishNumber:number = 3//每天一共可以发布多少篇文章
-    private curNumber:number = 0//当天可以发布的文章数量
+    private _publishNumber:number = 3//每天一共可以发布多少篇文章
+    private _curNumber:number = 0//当天可以发布的文章数量
 
-    constructor(public name:string = 'tom',public age:number = 10) {
+    constructor(public name:string = 'tom',private _age:number = 10) {
         this.id = Math.random()
     }
+
+    set age(value:number) {
+        if(value < 0) {
+            this._age = 0
+        }else if(value >= 200) {
+            this._age = 200
+        }else {
+            this._age = value;
+        }
+    }
+
+    get age(){
+        return Math.floor(this._age)
+    }
+
     publish(title:string) {
-        if(this.curNumber < this.publishNumber) {
+        if(this._curNumber < this._publishNumber) {
             console.log('发布了一篇文章:' + title);
-            this.curNumber ++;
+            this._curNumber ++;
         }else {
             console.log('今天发布的文章数量已达上限！')
         }
@@ -25,7 +40,9 @@ class User {
 }
 
 const u = new User('aa',12)
-console.log(u);
+// console.log(u);
+u.age =1.5;
+console.log(u.age);
 u.publish('文章1');
 u.publish('文章2');
 u.publish('文章3');
