eb0df59e196cceb582f1a88003db83fbb14b9ee0
zhichengyuan <1798788388@qq.com>
2021-07-15 11:59:24 +0800

    yzc

--- TS-fuxi/src/index.ts
+++ TS-fuxi/src/index.ts
@@ -1,20 +1,13 @@
-// import { createDeck, printDeck } from "./funcs";
-// const deck = createDeck();
-// printDeck(deck);
+function take<T>(arr:T[],n:number):T[] {
+    if(n >= arr.length) {
+        return arr;
+    }
+    const newArr:T[] = [];
+    for(let i =0;i<n;i++) {
+        newArr.push(arr[i]);
+    }
+    return newArr
+}
 
-import { Deck } from "./deck";
-
-let deck = new Deck
-deck.shuffle();
-console.log('--------洗牌之后----------')
-deck.print();
-const result = deck.publish()
-console.log('--------发牌之后----------')
-console.log('--------玩家1----------')
-result.player1.print()
-console.log('--------玩家2----------')
-result.player2.print()
-console.log('--------玩家3----------')
-result.player3.print()
-console.log('--------剩余牌----------')
-result.left.print()
+const newArr = take<number>([3,4,12,3,34,2],2)
+console.log(newArr)
