74ba11c084ab33ddd2500f9f834e1f01368fad3c
zhichengyuan <1798788388@qq.com>
2021-07-20 12:05:14 +0800

    yzc

--- TS-fuxi/src/index.ts
+++ TS-fuxi/src/index.ts
@@ -1,40 +1,92 @@
-export class Tank {
-    x:number = 0
-    y:number = 0
-    zidanSpeed:number =12
-    protected name:string = '坦克'
+abstract class Chess {
+    x:number =0;
+    y:number = 0;
+    abstract readonly name:string;
 
-    shoot() {
-        console.log('发射子弹')
+    move(targetX:number,targetY:number):boolean{
+        console.log('1.边界判断');
+        console.log('2.目标位置是否有己方棋子');
+        //3. 规则判断
+        if(this.rule(targetX,targetY)) {
+            this.x = targetX;
+            this.y = targetY;
+
+            console.log(`${this.name}移动成功了`)
+            return true;
+        }
+        return false
     }
 
-    sayHello() {
-        console.log(this.name)
+    protected isOutSide() : boolean {
+        console.log('1.边界判断')
+        return false
     }
-}
 
-export class PlayerTank extends Tank {
-    x:number = 20
-    y:number = 3
-    name:string ='玩家坦克'
-    life:number =5
-    shoot(){
-        console.log('玩家坦克发射子弹')
+    protected targetHesMengyou() {
+        console.log('2.目标位置是否有己方棋子')
+        return false;
     }
-    test(){
-        super.sayHello()
+
+    protected finishMove(targetX:number,targetY:number){
+        this.x = targetX;
+        this.y = targetY;
+        console.log('移动成功')
     }
+
+    protected abstract rule(targetX:number,targetY:number) :boolean ;
 }
 
-export class EnemyTank extends Tank {
+class Horse extends Chess {
+    protected rule(targetX: number, targetY: number): boolean {
+        return false
+    }
+  
+    name: string= '马'
 
 }
 
-const p = new PlayerTank()
-console.log(p.x)
-p.shoot()
-p.test()
+class Pao extends Chess {
+    protected rule(targetX: number, targetY: number): boolean {
+        return true
+    }
+   
+    name:string;
+    constructor () {
+        
+        super();
+        this.name = '炮'
+    }
 
-if(p instanceof PlayerTank) {
-    console.log(p.life)
 }
+
+class Soldier extends Chess {
+    protected rule(targetX: number, targetY: number): boolean {
+        return true
+    }
+   
+    get name() {
+        return '兵'
+    }
+
+}
+
+// const a = new Chess()
+const a = new Horse()
+const b = new Pao()
+const c = new Soldier()
+a.move(5,7)
+b.move(4,6)
+c.move(2,3)
+console.log(c.x);
+console.log(a.name,b.name,c.name)
+
+// class King extends Chess{
+//     name: string;
+//     protected rule(targetX: number, targetY: number): boolean {
+//         throw new Error("Method not implemented.");
+//     }
+    
+// }
+
+
+
