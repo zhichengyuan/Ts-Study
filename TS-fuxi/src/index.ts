394498c4370cabe7b1b592f29df1634745b3b888
zhichengyuan <1798788388@qq.com>
2021-07-29 23:44:36 +0800

    yzc

--- TS-fuxi/src/index.ts
+++ TS-fuxi/src/index.ts
@@ -1,15 +1,6 @@
-interface IUser {
-    name:string,
-    age:number,
-    sayHello(this:IUser):void
+class User {
+    loginid:string = '';//描述是：账号，验证规则：1.必填。2，必须3-5个字符
+    loginpwd:string = '12232';//必须是6-12个字符
+    age:number =2 //必须是0-100之间的数字
+    gendr:'男'|'女' = '男';
 }
-
-const u:IUser = {
-    name:'sssf',
-    age:33,
-    sayHello() {
-        console.log(this.name,this.age)
-    }
-}
-
-const say = u.sayHello();
