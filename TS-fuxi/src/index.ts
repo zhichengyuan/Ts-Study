fc54cf62dea7cb3aa20fea8d19addf96c8605661
zhichengyuan <1798788388@qq.com>
2021-07-31 11:01:16 +0800

    yzc

--- TS-fuxi/src/index.ts
+++ TS-fuxi/src/index.ts
@@ -1,6 +1,33 @@
-class User {
-    loginid:string = '';//描述是：账号，验证规则：1.必填。2，必须3-5个字符
-    loginpwd:string = '12232';//必须是6-12个字符
-    age:number =2 //必须是0-100之间的数字
-    gendr:'男'|'女' = '男';
+type constructer = new (...args:any[]) => object;
+function test(target:constructer) {
+    // console.log(target);
+}
+function test1(str:string) {
+    return function (target:constructer) {
+
+    }
+}
+function d1() {
+    console.log('d1')
+    return function (target:constructer) {
+        console.log('d1 decorator')
+    }
+}
+function d2() {
+    console.log('d2')
+    return function (target:constructer) {
+        console.log('d2 decorator')
+    }
+}
+
+@test1('这是一个字符串')
+@test
+class A {
+
+}
+
+@d1()
+@d2()
+class B {
+
 }
